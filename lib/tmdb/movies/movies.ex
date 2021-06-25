766391ec474ee6f6d7e1ccb24ecbbf46bb1625c3
Ruin0x11 <ipickering2@gmail.com>
2021-06-24 23:50:21 -0700

    Don't override params

--- lib/tmdb/movies/movies.ex
+++ lib/tmdb/movies/movies.ex
@@ -1,6 +1,6 @@
 defmodule Tmdb.Movies do
-  def find(client, id) do
-    Tesla.get!(client, "movie/#{id}", query: [append_to_response: "trailers"]).body
+  def find(client, id, params \\ []) do
+    Tesla.get!(client, "movie/#{id}", query: params).body
   end
 
   def genres(client) do
@@ -12,27 +12,27 @@ defmodule Tmdb.Movies do
   end
 
   def latest(client, params \\ []) do
-    Tesla.get!(client, "movie/latest", query: Keyword.put(params, :append_to_response, "videos")).body
+    Tesla.get!(client, "movie/latest", query: params).body
   end
 
   def now_playing(client, params \\ []) do
-    Tesla.get!(client, "movie/now_playing", query: Keyword.put(params, :append_to_response, "videos")).body
+    Tesla.get!(client, "movie/now_playing", query: params).body
   end
 
   def popular(client, params \\ []) do
-    Tesla.get!(client, "movie/popular", query: Keyword.put(params, :append_to_response, "videos")).body
+    Tesla.get!(client, "movie/popular", query: params).body
   end
 
   def similar(client, id, params \\ []) do
-    Tesla.get!(client, "movie/#{id}/similar", query: Keyword.put(params, :append_to_response, "videos")).body
+    Tesla.get!(client, "movie/#{id}/similar", query: params).body
   end
 
   def top(client, params \\ []) do
-    Tesla.get!(client, "movie/top_rated", query: Keyword.put(params, :append_to_response, "videos")).body
+    Tesla.get!(client, "movie/top_rated", query: params).body
   end
 
   def upcoming(client, params \\ []) do
-    Tesla.get!(client, "movie/upcoming", query: Keyword.put(params, :append_to_response, "videos")).body
+    Tesla.get!(client, "movie/upcoming", query: params).body
   end
 
   def videos(client, id, params \\ []) do
